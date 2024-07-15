9d1309e5de695c62385b86aa69fe54d1575621fa
User <gavinbelson@skiff.com>
2024-07-15 21:28:35 +0800

    Update metadata

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/hk-bus",
   images: {
     unoptimized: process.env.NODE_ENV == "development",
   },
